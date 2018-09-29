69dd2ab55b398ba09e5ce0d792bdb286b61f046f
dulltz <isrgnoe@gmail.com>
2018-09-29 17:12:40 +0900

    Add letter back

--- letter-generator/web/main.py
+++ letter-generator/web/main.py
@@ -7,9 +7,20 @@ def toppage():
     return render_template('index.html')
 
 
-@app.route("/letter", methods=['GET'])
-def letter():
-    return render_template('letter.html',
+@app.route("/letter_front", methods=['GET'])
+def letter_front():
+    return render_template('letter_front.html',
+                           postal_code=request.args.get('postal_code'),
+                           message=request.args.get('message'),
+                           receiver_name=request.args.get('receiver_name'),
+                           receiver_address=request.args.get(
+                               'receiver_address'),
+                           sender_name=request.args.get('sender_name'),
+                           sender_address=request.args.get('sender_address'))
+
+@app.route("/letter_back", methods=['GET'])
+def letter_back():
+    return render_template('letter_back.html',
                            postal_code=request.args.get('postal_code'),
                            message=request.args.get('message'),
                            receiver_name=request.args.get('receiver_name'),
