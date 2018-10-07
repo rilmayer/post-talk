87924b933f9d106e6f09762e3366dce7cf2afdf1
dulltz <isrgnoe@gmail.com>
2018-10-07 10:39:12 +0900

    print on both sides

--- letter-generator/capture/index.js
+++ letter-generator/capture/index.js
@@ -1,19 +1,15 @@
-'use strict';
-
 const AWS = require('aws-sdk');
 const launchChrome = require('@serverless-chrome/lambda');
 const CDP = require('chrome-remote-interface');
 const puppeteer = require('puppeteer');
 const uuidv1 = require('uuid/v1');
 
-const SAVE_BUCKET_NAME = 'postalk-dev';
+const SAVE_BUCKET_NAME = 'postalk.dev';
 
 exports.handler = async (event, context, callback) => {
     let slsChrome = null;
     let browser = null;
     let page = null;
-
-    await setFontConfig();
 
     try {
         const data = JSON.parse(event.body);
@@ -23,16 +19,16 @@ exports.handler = async (event, context, callback) => {
             browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl
         });
         page = await browser.newPage();
-
-        await page.goto('https://postalk-letter-dev.herokuapp.com/letter_front?message=' + data.message.join('\n') +
-            "?sender_name=" + data.sender_name +
-            "?sender_address=" + data.sender_address +
-            "?sender_postal_code=" + data.sender_postal_code +
-            "?receiver_name=" + data.receiver_name +
-            "?receiver_address=" + data.receiver_address +
-            "?reverver_postal_code=" + data.receiver_postal_code, {
-                waitUntil: 'domcontentloaded'
-            });
+        const url = 'https://postalk-letter-dev.herokuapp.com/letter_front?message=' + data.message.join('\n') +
+            "&sender_name=" + data.sender_name +
+            "&sender_address=" + data.sender_address +
+            "&sender_postal_code=" + data.sender_postal_code +
+            "&receiver_name=" + data.receiver_name +
+            "&receiver_address=" + data.receiver_address +
+            "&reverver_postal_code=" + data.receiver_postal_code
+        await page.goto(url, {
+            waitUntil: 'domcontentloaded'
+        });
         // await page.evaluate(() => {
         //     var style = document.createElement('style');
         //     style.textContent = `
@@ -45,26 +41,45 @@ exports.handler = async (event, context, callback) => {
         //         el.style.fontFamily = "'Source Code Pro', 'Noto Sans JP', monospace";
         //     });
         // });
-        await page.waitFor(1000);
         const pdfBuf = await page.pdf({
+            printBackground: true
+        });
+
+        await page.goto('https://postalk-letter-dev.herokuapp.com/letter_back?message=' + data.message.join('\n') +
+            "?sender_name=" + data.sender_name +
+            "?sender_address=" + data.sender_address +
+            "?sender_postal_code=" + data.sender_postal_code +
+            "?receiver_name=" + data.receiver_name +
+            "?receiver_address=" + data.receiver_address +
+            "?reverver_postal_code=" + data.receiver_postal_code, {
+                waitUntil: 'domcontentloaded'
+            });
+        const pdfBuf2 = await page.pdf({
             printBackground: true
         });
 
         const s3 = new AWS.S3();
         const fileName = "postalk-" + uuidv1() + '.pdf';
-        let s3Param = {
+        await s3.putObject({
             ACL: "public-read",
             Bucket: SAVE_BUCKET_NAME,
             Key: fileName,
             Body: pdfBuf
-        };
-        await s3.putObject(s3Param).promise();
+        }).promise();
+        const fileName2 = "postalk-" + uuidv1() + '.pdf';
+        await s3.putObject({
+            ACL: "public-read",
+            Bucket: SAVE_BUCKET_NAME,
+            Key: fileName2,
+            Body: pdfBuf2
+        }).promise();
 
         return callback(null, {
             "statusCode": 200,
             "body": JSON.stringify({
                 result: 'OK',
-                link: "https://s3.amazonaws.com/" + SAVE_BUCKET_NAME + "/" + fileName
+                front: "https://s3.amazonaws.com/" + SAVE_BUCKET_NAME + "/" + fileName,
+                back: "https://s3.amazonaws.com/" + SAVE_BUCKET_NAME + "/" + fileName2
             }),
             "isBase64Encoded": false
         });
@@ -92,16 +107,3 @@ exports.handler = async (event, context, callback) => {
         }
     }
 };
-
-function setFontConfig() {
-    return new Promise((resolve, reject) => {
-        process.env.HOME = process.env.LAMBDA_TASK_ROOT
-        const command = `fc-cache -v ${process.env.HOME}.fonts`
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                return reject(error)
-            }
-            resolve();
-        })
-    });
-}
